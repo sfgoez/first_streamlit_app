7ca8052cd078574cd1873f136f89b1d6fc9a4b06
SFG <103813308+sfgoez@users.noreply.github.com>
2022-04-17 09:44:19 +0200

    Update streamlit_app.py
    Corrected the text acording to the Snowflake example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
-streamlit.header('This is a header')
-streamlit.text('An example text')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
