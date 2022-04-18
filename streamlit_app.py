6d09b596c5639b2c03029d1e8d7c70b40850ba70
SFG <103813308+sfgoez@users.noreply.github.com>
2022-04-18 07:26:45 +0200

    Restored to excercise

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so the can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-# Show location
-pandas.dataframe.location[('Fruit')]
-
 # Show input box
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
