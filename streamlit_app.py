7be7f9f7d0ab5247dcc962951332f69359c0777b
SFG <103813308+sfgoez@users.noreply.github.com>
2022-04-18 07:14:50 +0200

    Experiment with the Panda loc function

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,12 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so the can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
+# Show location
+df.loc[('Cherries')]
+
+# Show input box
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display the table on the page
+# Display the table on the page
 streamlit.dataframe(fruits_selected)
